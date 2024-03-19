9995798c397dd3cc476e94a5bd350c8ab7b4d671
Francis Barre <franciib@stud.ntnu.no>
2024-03-19 11:38:15 +0100

    updated paths

--- src/make_training_data.py
+++ src/make_training_data.py
@@ -1,18 +1,24 @@
+# %%
 from get_label_data.get_labels import get_labels, save_labels
 from get_label_data.cut_images import cut_geotiff, save_cut_geotiff
 from pathlib import Path
+import geopandas as gpd
 
 # rectangle in the center in of trondheim
 bbox = [10.3281, 63.3805, 10.4901, 63.4325]
 
 # get the labels
-data, transform = get_labels(bbox, 0.3)
+root_dir = Path(__file__).parents[1]
+path_label = (root_dir / "data/raw/FKB_bygning" /
+              "Basisdata_5001_Trondheim_5972_FKB-Bygning_FGDB.gdb")
+gdf_omrade = gpd.read_file(path_label, driver="FileGDB",
+                           layer="fkb_bygning_omrade")
+data, transform = get_labels(gdf_omrade, bbox, 0.3)
 
 # save the labels
 save_labels(data, "trondheim_2019_rect_labels", transform)
 
 # get the image
-root_dir = Path(__file__).parent
 subfolder = "data/raw/orthophoto/res_0.3/trondheim_2019/i_lzw_25"
 geotiff_path = root_dir / subfolder / "Eksport-nib_4326.tif"
 image = cut_geotiff(geotiff_path, bbox, 0.3)
