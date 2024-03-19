e7597a8a8477fdfeb2c0adc1ac40b171f1d72091
Francis Barre <franciib@stud.ntnu.no>
2024-03-19 09:48:27 +0100

    updated dowload_orthophoto

--- src/download_orthophoto.py
+++ src/download_orthophoto.py
@@ -1,16 +1,17 @@
-import os
+from pathlib import Path
 import json
 import shutil
 
 from get_orthophoto.download_project import download_project
 
-possible_downloads = os.listdir(os.getcwd() + "/data/temp/urls/")
+root_directory = Path(__file__).parents[1]
+possible_downloads = root_directory / "data/temp/urls/"
 possible_downloads = [p for p in possible_downloads if "." in p]
 print(f'current urls: {possible_downloads}.')
 if len(possible_downloads) > 0:
     current_download = possible_downloads[0]
     # read in json:
-    job_path = os.getcwd() + "/data/temp/urls/"
+    job_path = root_directory / "data/temp/urls/"
     with open(job_path+current_download, "r") as f:
         job_details = json.load(f)
     for key in job_details:
@@ -19,4 +20,5 @@ if len(possible_downloads) > 0:
     # download the project
     download_project(**job_details)
     # move the job to archive
-    shutil.move(job_path+current_download, job_path+"used_urls/"+current_download)
+    shutil.move(job_path+current_download, job_path+"used_urls/" +
+                current_download)
