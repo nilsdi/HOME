262104fe7fb5f588b1c6eca63dbd673d6a819af7
Francis Barre <franciib@stud.ntnu.no>
2024-03-19 11:50:51 +0100

    added tqdm to get labels

--- src/make_training_data.py
+++ src/make_training_data.py
@@ -7,15 +7,16 @@ import geopandas as gpd
 # rectangle in the center in of trondheim
 bbox = [10.3281, 63.3805, 10.4901, 63.4325]
 
-# get the labels
+# %% get the labels
 root_dir = Path(__file__).parents[1]
 path_label = (root_dir / "data/raw/FKB_bygning" /
               "Basisdata_5001_Trondheim_5972_FKB-Bygning_FGDB.gdb")
 gdf_omrade = gpd.read_file(path_label, driver="FileGDB",
                            layer="fkb_bygning_omrade")
+# %%
 data, transform = get_labels(gdf_omrade, bbox, 0.3)
 
-# save the labels
+# %% save the labels
 save_labels(data, "trondheim_2019_rect_labels", transform)
 
 # get the image
