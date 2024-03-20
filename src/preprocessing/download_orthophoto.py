9e27d396bc56f45206c789035959c79a33ea153d
Francis Barre <franciib@stud.ntnu.no>
2024-03-20 10:35:52 +0100

    tqdm for download

--- src/preprocessing/download_orthophoto.py
+++ src/preprocessing/download_orthophoto.py
@@ -2,18 +2,21 @@
 from pathlib import Path
 import json
 import shutil
+import sys
 
-from get_orthophoto.download_project import download_project
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+from get_orthophoto.download_project import download_project  # noqa
 
 root_directory = Path(__file__).parents[2]
 possible_downloads = root_directory.glob("data/temp/urls/*")
 possible_downloads = [p for p in possible_downloads if "." in str(p)]
 print(f'current urls: {possible_downloads}.')
-if len(possible_downloads) > 0:
-    current_download = possible_downloads[0]
+
+# %%
+for current_download in possible_downloads:
     # read in json:
     job_path = root_directory / "data/temp/urls/"
-    with open(job_path / current_download, "r") as f:
+    with open(current_download, "r") as f:
         job_details = json.load(f)
     for key in job_details:
         print(f'{key}: {job_details[key]}')
@@ -21,7 +24,7 @@ if len(possible_downloads) > 0:
     # download the project
     download_project(**job_details)
     # move the job to archive
-    shutil.move(job_path / current_download, job_path / "used_urls/" /
-                current_download)
+    shutil.move(current_download, job_path / "used_urls" /
+                current_download.name)
 
 # %%
