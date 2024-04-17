93f265ddf84206b3136773f832fab463a2c1386a
Francis Barre <franciib@stud.ntnu.no>
2024-04-17 18:29:19 +0200

    added viz and env file

--- src/utils/display_tiff.py
+++ src/utils/display_tiff.py
@@ -141,11 +141,13 @@ display_rgb_geotiff_subset(file_path, 0, 0, 28000, 17000)
 # %% display image, label and prediction side to side
 
 def display_images_side_by_side(
-        name, prediction: bool = False,
+        name, prediction: bool = False, label: bool = True,
         prediction_folder=root_dir + "/data/model/original/predictions/",
         image_folder=root_dir + "/data/model/original/train/image/",
         label_folder=root_dir + "/data/model/original/train/label/"):
 
+    assert prediction or label, (
+        "At least one of prediction or label must be True")
     if name == 'random':
         if prediction:
             files_in_folder = [f for f in os.listdir(prediction_folder)]
@@ -168,15 +170,17 @@ def display_images_side_by_side(
     # Open the files
     if prediction:
         with (rasterio.open(image_path) as image,
-              rasterio.open(label_path) as label,
               rasterio.open(prediction_path) as prediction):
             # Read the data
             image_data = image.read([1, 2, 3])
-            label_data = label.read(1)
             prediction_data = prediction.read(1)
 
+            if label:
+                with rasterio.open(label_path) as label:
+                    label_data = label.read(1)
+
             # Create a figure with three subplots
-            fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+            fig, axs = plt.subplots(1, 3 if label else 2, figsize=(15, 5))
 
             # Display the image
             axs[0].imshow(image_data.transpose((1, 2, 0)))
@@ -184,17 +188,18 @@ def display_images_side_by_side(
             axs[0].set_xticks([])
             axs[0].set_yticks([])
 
-            # Display the label
-            axs[1].imshow(label_data, cmap='gray')
-            axs[1].set_title('Ground Truth')
-            axs[1].set_xticks([])
-            axs[1].set_yticks([])
+            if label:
+                # Display the label
+                axs[1].imshow(label_data, cmap='gray')
+                axs[1].set_title('Ground Truth')
+                axs[1].set_xticks([])
+                axs[1].set_yticks([])
 
             # Display the prediction
-            axs[2].imshow(prediction_data, cmap='gray')
-            axs[2].set_title('Prediction')
-            axs[2].set_xticks([])
-            axs[2].set_yticks([])
+            axs[-1].imshow(prediction_data, cmap='gray')
+            axs[-1].set_title('Prediction')
+            axs[-1].set_xticks([])
+            axs[-1].set_yticks([])
 
             # Show the figure
             plt.show()
@@ -225,12 +230,12 @@ def display_images_side_by_side(
     return (fig)
 
 
-image_folder = root_dir + "/data/model/original/train_poor/image/"
-prediction_folder = root_dir + "/data/model/original/predictions/BW_2023/"
+image_folder = root_dir + "/data/model/topredict/train/image/"
+prediction_folder = root_dir + "/data/model/topredict/predictions/BW_1937/"
 
 fig = display_images_side_by_side('random', prediction=True,
                                   prediction_folder=prediction_folder,
-                                  image_folder=image_folder)
+                                  image_folder=image_folder, label=False)
 
 # %%
 
@@ -375,4 +380,66 @@ def display_several_years(folder_images, folder_labels, folder_predictions,
 fig = display_several_years(folder_images, folder_labels, folder_predictions,
                             years, filenames)
 
+# %% Function to plot side by side image and different predictions
+
+
+def display_predictions(image_folder, prediction_folders: list,
+                        prediction_names: list,
+                        label_folder=None, name='random'):
+    if name == 'random':
+        files_in_folder = [f for f in os.listdir(prediction_folders[-1])]
+        name = files_in_folder[random.randint(0, len(files_in_folder))]
+        image_path = image_folder + name
+        if label_folder is not None:
+            label_path = label_folder + name
+    # Open the files
+    with rasterio.open(image_path) as image:
+        # Read the data
+        image_data = image.read([1, 2, 3])
+
+        # Create a figure with two subplots
+        fig, axs = plt.subplots(1, len(prediction_folders) + 2 if label_folder
+                                is not None else len(prediction_folders) + 1,
+                                figsize=(5 * len(prediction_folders), 5))
+
+        # Display the image
+        axs[0].imshow(image_data.transpose((1, 2, 0)))
+        axs[0].set_title('Image')
+        axs[0].set_xticks([])
+        axs[0].set_yticks([])
+
+        k = 1
+        if label_folder is not None:
+            with rasterio.open(label_path) as label:
+                # Read the data
+                label_data = label.read(1)
+                axs[1].imshow(label_data, cmap='gray')
+                axs[1].set_title('Ground Truth')
+                axs[1].set_xticks([])
+                axs[1].set_yticks([])
+            k = 2
+
+        for i, prediction_folder in enumerate(prediction_folders):
+            prediction_path = prediction_folder + name
+            with rasterio.open(prediction_path) as prediction:
+                # Read the data
+                prediction_data = prediction.read(1)
+                axs[i + k].imshow(prediction_data, cmap='gray')
+                axs[i + k].set_title(f'Prediction {prediction_names[i]}')
+                axs[i + k].set_xticks([])
+                axs[i + k].set_yticks([])
+
+        # display name of the image
+        fig.suptitle(name, fontsize=16)
+
+        # Show the figure
+        plt.show()
+
+
+image_folder = root_dir + "/data/model/topredict/train/image/"
+prediction_folders = [root_dir + "/data/model/topredict/predictions/",
+                      root_dir + "/data/model/topredict/predictions/BW_1937/"]
+prediction_names = ['Original training', 'B&W training']
+
+display_predictions(image_folder, prediction_folders, prediction_names)
 # %%
