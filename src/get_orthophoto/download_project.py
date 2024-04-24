93c6ac23cca27e96e43eec79f73dff9f3b7e88d1
nilsdi <nils.dittrich@ntnu.no>
2024-04-24 16:52:02 +0200

    adjusted download procedure to rely on export detail jsons

--- src/get_orthophoto/download_project.py
+++ src/get_orthophoto/download_project.py
@@ -13,6 +13,10 @@ def download_project(download_url: str, project: str, resolution: float,
                      mosaic: bool) -> None:
     # Retrieve data
     response = requests.get(download_url, allow_redirects=True, stream=True)
+    print(f'we get status_code {response.status_code}.')
+    if response.status_code != 200:
+        raise Exception(f"Download request failed with status code {response.status_code}.")
+    
     total_size = int(response.headers.get('content-length', 0))
 
     # set up the path to save the file and unzip it
@@ -50,16 +54,20 @@ def download_project(download_url: str, project: str, resolution: float,
         # Remove the zip file after extraction
         os.remove(file_path)
 
-    # Check the CRS of the orthophoto
-    dataset = gdal.Open(str(unzip_folder / "Eksport-nib.tif"))
-    srs = osr.SpatialReference(wkt=dataset.GetProjection())
+    # find all tif files in the folder:
+    tif_files = [f for f in os.listdir(unzip_folder) if ".tif" in f]
+    for tif_file in tif_files:
+        # Check the CRS of the orthophoto
+        dataset = gdal.Open(str(unzip_folder / tif_file))
+        srs = osr.SpatialReference(wkt=dataset.GetProjection())
 
-    # Check if the CRS is already EPSG:25833
-    if srs.GetAuthorityCode(None) != "25833":
-        output_path = str(unzip_folder / "Eksport-nib_25833.tif")
-        # Warp the file to EPSG:25833
-        warp_options = gdal.WarpOptions(dstSRS='EPSG:25833')
-        gdal.Warp(output_path, dataset, options=warp_options)
-        # Remove the original file
-        # os.remove(unzip_folder / "/Eksport-nib.tif")
+        # Check if the CRS is already EPSG:25833
+        if srs.GetAuthorityCode(None) != "25833":
+
+            output_path = str(unzip_folder / tif_file)
+            # Warp the file to EPSG:25833
+            warp_options = gdal.WarpOptions(dstSRS='EPSG:25833')
+            gdal.Warp(output_path, dataset, options=warp_options)
+            # Remove the original file
+            # os.remove(unzip_folder / "/Eksport-nib.tif")
     return
