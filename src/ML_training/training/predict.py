3f49776dab64c9464815c3188ca6d320beb5f8b7
Francis Barre <franciib@stud.ntnu.no>
2024-06-12 11:31:47 +0200

    fixed predictions

--- src/ML_training/training/predict.py
+++ src/ML_training/training/predict.py
@@ -9,9 +9,9 @@ from torch.utils.data import DataLoader
 import cv2
 from tqdm import tqdm
 
-grandparent_dir = Path(__file__).parents[2]
-sys.path.append(str(grandparent_dir))
-sys.path.append(str(grandparent_dir / "ISPRS_HD_NET"))
+preroot_dir = Path(__file__).parents[4]
+sys.path.append(str(preroot_dir))
+sys.path.append(str(preroot_dir / "ISPRS_HD_NET"))
 from ISPRS_HD_NET.utils.sync_batchnorm.batchnorm import convert_model  # type: ignore # noqa
 from ISPRS_HD_NET.model.HDNet import HighResolutionDecoupledNet  # type: ignore # noqa
 from ISPRS_HD_NET.utils.dataset import BuildingDataset  # type: ignore # noqa
@@ -21,44 +21,28 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 matplotlib.use("tkagg")
 
 # %%
-root_dir = Path(__file__).parents[1]
+root_dir = Path(__file__).parents[3]
 # data_dir = str(root_dir) + "/data/model/topredict/"
-data_dir = str(root_dir) + "/data/model/trondheim_1979/"
+data_dir = str(root_dir) + "/data/ML_prediction/"
 
-dir_checkpoint = str(root_dir) + "/data/model/save_weights/run_3/"
+dir_checkpoint = str(root_dir) + "/data/ML_model/save_weights/run_3/"
 # dir_checkpoint = "../ISPRS_HD_NET/save_weights/pretrain/"
 predict = True
 # prediction_folder = 'predictions/BW_RGB_training/'
 # image_folder = 'train/image'
 
-prediction_folder = "predictions/test/"
-image_folder = "tiles/images"
+prediction_folder = "predictions/"
+image_folder = "topredict/image/"
+txt_file = "pred_trondheim_2019_0.3_i_lzw_25.txt"
 
 batchsize = 2
 num_workers = 16
 read_name = "HDNet_NOCI_best"
 # Dataset = 'NOCI'
-Dataset = "Inria"
+Dataset = "NOCI"
 # assert Dataset in ['WHU', 'Inria', 'Mass', 'NOCI']
 net = HighResolutionDecoupledNet(base_channel=48, num_classes=1)
 print("Number of parameters: ", sum(p.numel() for p in net.parameters()))
-
-dataset = BuildingDataset(
-    dataset_dir=data_dir,
-    training=False,
-    txt_name="test.txt",
-    data_name=Dataset,
-    image_folder=image_folder,
-    predict=predict,
-)
-
-# %%
-txt_path = os.path.join(data_dir, "dataset", "test.txt")
-with open(os.path.join(txt_path), "r") as f:
-    file_names = [x for x in f.readlines() if len(x.strip()) > 0]
-
-print(len(file_names))
-#
 
 
 # %%
@@ -74,7 +58,7 @@ def predict_and_eval(
     dataset = BuildingDataset(
         dataset_dir=data_dir,
         training=False,
-        txt_name="test.txt",
+        txt_name=txt_file,
         data_name=Dataset,
         image_folder=image_folder,
         predict=predict,
@@ -118,6 +102,7 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     logging.info(f"Using device {device}")
+
     if read_name != "":
         net_state_dict = net.state_dict()
         state_dict = torch.load(
